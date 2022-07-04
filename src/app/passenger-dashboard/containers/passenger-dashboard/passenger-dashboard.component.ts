c8c68afbbdde34f0bb7b78e26479a71772760835
IsaGutierrez <isagr_kio@hotmail.com>
2022-07-04 12:55:01 +0200

    Added OnInit hook.

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,9 +1,6 @@
-import { Component } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
 
 import { Passenger } from "../../models/passenger.interface";
-
-
 
 @Component({
     selector: 'passenger-dashboard',
@@ -11,7 +8,14 @@ import { Passenger } from "../../models/passenger.interface";
     templateUrl: 'passenger-dashboard.component.html'
 })
 
-export class PassengerDashboardComponent {
+export class PassengerDashboardComponent implements OnInit {
+    
+    constructor () {
+        this.title = 'Ultimate Angular'
+    }
+    
+    passengers: Passenger[] = [];
+
     title: string;
     numberOne: number = 1;
     numberTwo: number = 2;
@@ -19,52 +23,54 @@ export class PassengerDashboardComponent {
     logo: string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/512px-Angular_full_color_logo.svg.png'
     name: string = 'Isa'
     name1: string = 'Anass'
-
-    passengers: Passenger[] = [
-        {
-            id: 1,
-            name: 'Isa',
-            checkedIn: true,
-            checkInDate: 1655214039379,
-            pets: [
-                {
-                    name: 'Yami',
-                    age: 7
-                },
-                { 
-                    name: 'Kiara',
-                    age: 8
-                }
-            ]
-        },
-        {
-            id: 2,
-            name: 'Ana',
-            checkedIn: false,
-            checkInDate: null,
-            pets: null
-        },
-        {
-            id: 3,
-            name: 'Yami',
-            checkedIn: true,
-            checkInDate: 1655214039379,
-            pets: null
-        },
-        {
-            id: 4,
-            name: 'Link',
-            checkedIn: false,
-            checkInDate: null,
-            pets: [
-                {
-                    name: 'Epona',
-                    age: 10
-                }
-            ]
-        },
-
-    ]
+        
+    ngOnInit() {
+        this.passengers =  [
+            {
+                id: 1,
+                name: 'Isa',
+                checkedIn: true,
+                checkInDate: 1655214039379,
+                pets: [
+                    {
+                        name: 'Yami',
+                        age: 7
+                    },
+                    { 
+                        name: 'Kiara',
+                        age: 8
+                    }
+                ]
+            },
+            {
+                id: 2,
+                name: 'Ana',
+                checkedIn: false,
+                checkInDate: null,
+                pets: null
+            },
+            {
+                id: 3,
+                name: 'Yami',
+                checkedIn: true,
+                checkInDate: 1655214039379,
+                pets: null
+            },
+            {
+                id: 4,
+                name: 'Link',
+                checkedIn: false,
+                checkInDate: null,
+                pets: [
+                    {
+                        name: 'Epona',
+                        age: 10
+                    }
+                ]
+            },
+        ]
+        
+    }
 
     handleChange(value: string) {
         this.name = value
@@ -79,7 +85,5 @@ export class PassengerDashboardComponent {
 
     }
 
-    constructor() {
-        this.title = 'Ultimate Angular'
-    }
+
 }
